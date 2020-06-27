6f6c3825b96bb9b02e2703d6113cd2226e1d2eab
Joseph Fiore <98jfiore@gmail.com>
2020-06-27 13:07:14 -0400

    Updates

---
+++ precache-manifest.623865b19628b96d1fc9221865da9107.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3714c73bbd77c8aa8e7beeb6055ddaf",
+    "url": "/react_mysite/index.html"
+  },
+  {
+    "revision": "3bc7baddad9d5f7ef120",
+    "url": "/react_mysite/static/css/main.cbe7cd8d.chunk.css"
+  },
+  {
+    "revision": "a24f0527c2ee61e72f10",
+    "url": "/react_mysite/static/js/2.ae3d053a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_mysite/static/js/2.ae3d053a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc7baddad9d5f7ef120",
+    "url": "/react_mysite/static/js/main.b34fec6d.chunk.js"
+  },
+  {
+    "revision": "886e6d8777ed8e76ac98",
+    "url": "/react_mysite/static/js/runtime-main.5ae8b2f4.js"
+  },
+  {
+    "revision": "7a984ce56151c60efb2fe283254220d9",
+    "url": "/react_mysite/static/media/banner.7a984ce5.jpg"
+  },
+  {
+    "revision": "053ec4e92ff963b22b3fb41995891cde",
+    "url": "/react_mysite/static/media/mobileUI-site-nav-opener-bg.053ec4e9.svg"
+  }
+]);
