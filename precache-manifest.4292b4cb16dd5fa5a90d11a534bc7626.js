ba4f713af08045c54c19b8dd2f6b6313415e7127
Joseph Fiore <98jfiore@gmail.com>
2020-08-16 16:03:31 -0400

    Updates

---
+++ precache-manifest.4292b4cb16dd5fa5a90d11a534bc7626.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca5825dcd6c4b712a35a8c98e4645b1",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "aa4ac264e747658a78ff",
+    "url": "/homepage/static/css/main.a6fcc5c5.chunk.css"
+  },
+  {
+    "revision": "bfd83ac648b02334af05",
+    "url": "/homepage/static/js/2.95a249a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homepage/static/js/2.95a249a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4ac264e747658a78ff",
+    "url": "/homepage/static/js/main.336263fe.chunk.js"
+  },
+  {
+    "revision": "46934735b42e5a96d838",
+    "url": "/homepage/static/js/runtime-main.dca32799.js"
+  },
+  {
+    "revision": "7a984ce56151c60efb2fe283254220d9",
+    "url": "/homepage/static/media/banner.7a984ce5.jpg"
+  },
+  {
+    "revision": "053ec4e92ff963b22b3fb41995891cde",
+    "url": "/homepage/static/media/mobileUI-site-nav-opener-bg.053ec4e9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.860d89f3cf49871234112be7804e10ee.js"
+  "/homepage/precache-manifest.4292b4cb16dd5fa5a90d11a534bc7626.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
